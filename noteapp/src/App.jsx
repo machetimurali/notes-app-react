2d34f2b10c9cd220da730ae6dbeba21d720741b7
machetimurali <muralimacheti15198@gmail.com>
2023-09-18 19:45:22 +0530

    init

---
+++ noteapp/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import './App.css'
+import Notes from "./components/Notes"
+
+function App() {
+
+  return (
+    <div className="main">
+       <Header/>
+       <Notes/>
+    </div>
+  )
+}
+
+export default App
